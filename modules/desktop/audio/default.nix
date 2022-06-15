2840b9df257fefd409c5e06e7b8e56fb02a7f876
Samuel Kuehnhold <smkuehnhold@gmail.com>
2022-06-15 00:26:31 +0000

    Fixes issue where there is no audio upon login

--- modules/desktop/audio/default.nix
+++ modules/desktop/audio/default.nix
@@ -3,4 +3,9 @@
 {
   # FIXME: At some point, I want a native pipewire cli client...
   home.packages = with pkgs; [ pamixer pulseaudio ];
+
+  # FIXME: Pipewire is borked after startup for some reason
+  xsession.initExtra = ''
+    systemctl --user restart pipewire
+  '';
 }
