61ef2a4dda8b31b9a8d8ba007731af44325f2a3f
Samuel Kuehnhold <smkuehnhold@gmail.com>
2022-09-28 21:06:29 -0400

    stuff

---
+++ modules/desktop/environment/bspwm+sxhkd/bspwm/startup.sh
@@ -0,0 +1,9 @@
+. get_active_monitors >> /dev/null
+
+for monitor in ${C_ACTIVE_MONITORS[@]} do
+  if [ "$monitor" = "DP-1" ]; then 
+    xrandr --output "DP-1" --primary
+    xrandr --output "eDP-1" --same-as "DP-1"
+    break
+  fi
+done
