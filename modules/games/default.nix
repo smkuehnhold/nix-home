b25abe529b19fc681b2875b4257baeca480b03dd
Samuel M. Kuehnhold <smkuehnhold@gmail.com>
2023-04-01 03:29:49 -0400

    Fixes lutris issue with battlenet. Moves font out of home environment and into lutris closure.

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -8,8 +8,12 @@
 
   config = {
     home.packages = with pkgs; [ 
-      lutris-free 
-      gnome3.adwaita-icon-theme # for lutris font
+      (lutris-free.override {
+        extraPkgs = pkgs: [ 
+          pkgs.libnghttp2 # fixes battlenet bug https://github.com/NixOS/nixpkgs/issues/214375
+          pkgs.gnome3.adwaita-icon-theme # for lutris font
+        ];
+      })
       steam
     ];
   };
