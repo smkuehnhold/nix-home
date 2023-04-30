652a8646d2990e1025ab24d1eb1cf1aea90f36af
Samuel M. Kuehnhold <smkuehnhold@gmail.com>
2023-04-30 19:34:06 -0400

    Temporarily upgrades to unstable lutris to fix battlenet issues

--- modules/games/lutris/default.nix
+++ modules/games/lutris/default.nix
@@ -3,7 +3,7 @@
 with lib;
 
 mkIf (builtins.elem "lutris" config.my.games.suites) {
-  home.packages = with pkgs; [ (lutris-free.override {
+  home.packages = with pkgs.unstable; [ wine-staging (lutris-free.override {
     extraPkgs = pkgs: [ 
       pkgs.libnghttp2 # fixes battlenet bug https://github.com/NixOS/nixpkgs/issues/214375
       pkgs.gnome3.adwaita-icon-theme # for lutris font
