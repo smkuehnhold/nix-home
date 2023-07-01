29e24beba3f514ae2cd9c84ff376aa304701467c
Samuel M. Kuehnhold <smkuehnhold@gmail.com>
2023-06-30 22:02:24 -0400

    Modifies steam closure to include ncurses6. Fixes linker error when launching CK3 via Steam/Paradox launcher

--- modules/games/steam/default.nix
+++ modules/games/steam/default.nix
@@ -3,5 +3,11 @@
 with lib;
 
 mkIf (builtins.elem "steam" config.my.games.suites) {
-  home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    # fixes ck3 linker error
+    # https://forum.paradoxplaza.com/forum/threads/ck-iii-game-refuses-to-launch-with-steam-runtime-environment-on-linux-1-8-0.1560341/#post-28974552
+    (steam.override {
+      extraPkgs = pkgs: [ pkgs.ncurses6 ];
+    })
+  ];
 }
